a9615062d28c779c1505e4e5560036065d610d82
ebbnormal <Ryan.Wieghard@gmail.com>
2016-02-28 22:18:48 -0800

    makes site navigable.

---
+++ app/assets/javascripts/klein.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+  $(".new_score_submit").click(function(){
+  	
+    $(".waiting").css("display", "block");
+  });
+
+
+
+});
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160229024347) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "generators", force: true do |t|
     t.float    "event1_mean"
     t.float    "event1_variance"
